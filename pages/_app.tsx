af25a8bdc674ec069f94a47cb3cc7b2b6a9fd2e7
IgHez <109860992+IgHez@users.noreply.github.com>
2023-04-23 18:47:49 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChainId ,ThirdwebProvider } from "@thirdweb-dev/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-  <ThirdwebProvider>
     <Component {...pageProps} />
-  </ThirdwebProvider>
   )
 }
